f1f009a63c5b5d14568a87e2eeedf9ae393586f6
gordonchen592 <gordon.chen592@utexas.edu>
2023-11-10 11:01:15 -0600

    update pydoc strings + add switch_turn and update_position

--- sticks_suggestions/algorithm.py
+++ sticks_suggestions/algorithm.py
@@ -1,26 +1,45 @@
 import numpy as py
-import hands
 
 class Sim_Game:
     def __init__(self):
         self.hand_dict = {"p1":{"l":1, "r":1}, "p2":{"l":1, "r":1}}
-        self.person_turn = "p1"
-        self.person_next_turn = "p2"
-        pass
+        self.player_turn = "p1"
+        self.player_turn_next = "p2"
 
-    def move(self,used_hand=None, target_hand=None, Right_hand=None,type="tap"):
+    def switch_turn(self, player_turn=None):
+        if player_turn:
+            self.player_turn = "p1"
+            self.player_turn_next = "p2" if player_turn=="p1" else "p1"
+    
+    def update_position(self, hand_dict_, player_turn=None):
+        self.hand_dict = hand_dict_
+        if player_turn:
+            self.switch_turn(player_turn)
+    
+    def move(self,used_hand=None, target_hand=None, rh_val=None, type="tap"):
         '''
-        Calculates the intended move.
+        Calculates the intended move and performs it.
 
-        Optional Parameters:
-        used_hand: "l" or "r" 
-        target_hand: "l" or "r" 
-        Right_hand: 0 to 4 (Step = 1)
+        Parameters
+        ----------
+            used_hand: char, optional
+                The hand used by the player. {"l", "r", None} (Default is None in "split" mode and "r" in "tap" mode)
+            target_hand: char, optional
+                The hand targeted by the player to be hit. {"l", "r", None} (Default is None in "split" mode and "r" in "tap" mode)
+            rh_val: int, optional
+                The intended count of sticks on the player's right hand after splitting when in "split" mode. An integer 0 through 4, or None (Default is None in "tap"mode and "1" in split mode)
+            type: str, optional
+                The type of move. {"tap","split"} (Default is "tap")
+        Returns
+        -------
+            boolean
+                True if a valid move is made
+                False if an invalid move is made
         '''
         if type == "tap":
-            if (0 < self.hand_dict[self.person_turn][used_hand] < 5) and (0 < self.hand_dict[self.person_next_turn][target_hand] < 5):
-                self.hand_dict[self.person_next_turn][target_hand] += self.hand_dict[self.person_turn][used_hand]
-                self.person_turn, self.person_next_turn = self.person_next_turn, self.person_turn
+            if (0 < self.hand_dict[self.player_turn][used_hand] < 5) and (0 < self.hand_dict[self.player_turn_next][target_hand] < 5):
+                self.hand_dict[self.player_turn_next][target_hand] += self.hand_dict[self.player_turn][used_hand]
+                self.player_turn, self.player_turn_next = self.player_turn_next, self.player_turn
                 return True
             else:
                 return False
--- sticks_suggestions/hands.py
+++ sticks_suggestions/hands.py
@@ -2,6 +2,33 @@ import cv2
 import mediapipe as mp
 
 class Hand:
+    '''
+    A class for mediapipe hand landmarking.
+    
+    ...
+    
+    Attributes
+    ----------
+    model_path : str
+        the path to the hand landmarker model
+    running_mode : mp.tasks.vision.RunningMode
+        the mediapipe running mode, {IMAGE, VIDEO, LIVESTREAM}
+    num_hands : int
+        the maximum number of hands to detect
+    min_hand_detection_confidence : float
+        The minimum confidence score for the hand detection to be considered successful in palm detection model.
+    min_hand_presence_confidence : float
+        The minimum confidence score for the hand presence score in the hand landmark detection model. In Video mode and Live stream mode, if the hand presence confidence score from the hand landmark model is below this threshold, Hand Landmarker triggers the palm detection model.
+    min_tracking_confidence : float
+        The minimum confidence score for the hand tracking to be considered successful. This is the bounding box IoU threshold between hands in the current frame and the last frame.
+    result_callback : function
+        Sets the result listener to receive the detection results asynchronously when the hand landmarker is in live stream mode.
+    
+    Methods
+    -------
+    find_hands(img):
+        Given an image, returns a dictionary containing the current game position.
+    '''
     
     def __init__(self, model_path="models/hand_landmarker.task",
                  running_mode="LIVE_STREAM",
@@ -10,6 +37,27 @@ class Hand:
                  min_hand_presence_confidence=0.5,
                  min_tracking_confidence=0.5,
                  result_callback=None):
+        '''
+        Constructs the attributes for the hand object
+        
+        Parameters
+        ----------
+            model_path : str, optional
+            the path to the hand landmarker model (Default is "models/hand_landmarker.task")
+            running_mode : str, optional
+                the mediapipe running mode, {"IMAGE", "VIDEO", "LIVESTREAM"}, (Default is "LIVESTREAM")
+            num_hands : int, optional 
+                the maximum number of hands to detect (Default is 4)
+            min_hand_detection_confidence : float, optional
+                The minimum confidence score for the hand detection to be considered successful in palm detection model. (Default is 0.5)
+            min_hand_presence_confidence : float, optional
+                The minimum confidence score for the hand presence score in the hand landmark detection model. In Video mode and Live stream mode, if the hand presence confidence score from the hand landmark model is below this threshold, Hand Landmarker triggers the palm detection model. (Default is 0.5)
+            min_tracking_confidence : float, optional
+                The minimum confidence score for the hand tracking to be considered successful. This is the bounding box IoU threshold between hands in the current frame and the last frame. (Default is 0.5)
+            result_callback : function, optional
+                Sets the result listener to receive the detection results asynchronously when the hand landmarker is in live stream mode. (Default is None)
+        '''
+        
         # init instance variables
         self.model_path = model_path
         match(running_mode):
@@ -40,7 +88,13 @@ class Hand:
     
     def find_hands(self,img):
         '''
-        Return a dictionary containing the current game position:
-        Example: {"player_1": {"left": 1, "right": 1}, "player_2": {"left": 1, "right": 1}}
+        Given an image, returns a dictionary containing the current game position.
+        
+            Parameters:
+                img (numpy_frame): image frame from OpenCV's video input
+                
+            Returns:
+                current_position (dict): dictionary containing the current game position. None if no hands detected.
+                Example: {"player_1": {"left": 1, "right": 1}, "player_2": {"left": 1, "right": 1}}
         '''
         pass
