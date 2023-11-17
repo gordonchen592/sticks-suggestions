9691e846b6fd31d27259f668362d860c3a8d3bac
jhale3141 <hale.joshua2019@utexas.edu>
2023-11-16 20:43:24 -0600

    Finish implementing negamax algorithm

--- sticks_suggestions/algorithm.py
+++ sticks_suggestions/algorithm.py
@@ -1,4 +1,5 @@
-import json
+import pickle
+from pathlib import Path
 
 class Sim_Game:
     def __init__(self):
@@ -55,47 +56,59 @@ class Sim_Game:
                 return True
             else:
                 return False
-        def is_p1_win(self):
-                return self.hand_dict["p2"]["l"] == self.hand_dict["p2"]["r"] == 0
-        def is_p2_win(self):
-                return self.hand_dict["p1"]["l"] == self.hand_dict["p1"]["r"] == 0
-        def is_end(self):
-                return self.is_p1_win() or self.is_p2_win() or self.is_repeating()
-        def get_gp_as_str(self):
-            return str(self.hand_dict["p1"]["l"]) + str(self.hand_dict["p1"]["r"]) + str(self.hand_dict["p2"]["l"]) + str(self.hand_dict["p2"]["r"])
-        def get_possible_moves(self):
-            possible_list = []
+    def is_p1_win(self):
+        return self.hand_dict["p2"]["l"] == self.hand_dict["p2"]["r"] == 0
+    def is_p2_win(self):
+        return self.hand_dict["p1"]["l"] == self.hand_dict["p1"]["r"] == 0
+    def is_end(self):
+        return self.is_p1_win() or self.is_p2_win()
+    def get_gp_as_str(self):
+        return str(self.hand_dict["p1"]["l"]) + str(self.hand_dict["p1"]["r"]) + str(self.hand_dict["p2"]["l"]) + str(self.hand_dict["p2"]["r"])
+    def get_possible_moves(self):
+        possible_list = []
 
-            # tapping
-            for used_hand in ['r', 'l']:
-                    if (0 < self.hand_dict[self.player_turn][used_hand] < 5) and (0 < self.hand_dict[self.player_turn_next][target_hand] < 5):
-                        possible_list.append(f"tap {used_hand} {target_hand}")
-            # splitting
-            for rh_val in range(0,5):
-                if sum(self.hand_dict[self.player_turn].values()) > 1 and self.hand_dict[self.player_turn]["l"] != rh_val:
-                    sticks_moved = self.hand_dict[self.player_turn]["r"] - rh_val
-                    if sticks_moved + self.hand_dict[self.player_turn]["l"] == 0 and rh_val == 0:
-                        continue
-                    possible_list.append("split {rh_val}")
+        # tapping
+        for used_hand in ['r', 'l']:
+            for target_hand in ['r','l']:
+                if (0 < self.hand_dict[self.player_turn][used_hand] < 5) and (0 < self.hand_dict[self.player_turn_next][target_hand] < 5):
+                    possible_list.append(f"tap {used_hand} {target_hand}")
+        
+        # splitting
+        for rh_val in range(0,5):
+            if sum(self.hand_dict[self.player_turn].values()) > 1 and self.hand_dict[self.player_turn]["l"] != rh_val:
+                sticks_moved = self.hand_dict[self.player_turn]["r"] - rh_val
+                if sticks_moved + self.hand_dict[self.player_turn]["l"] == 0 and rh_val == 0:
+                    continue
+                possible_list.append("split {rh_val}")
 
 class Algorithm:
     LOWERBOUND, EXACT, UPPERBOUND = -1, 0, 1
     inf = float("infinity")
 
-    def __init__(self,tt_path):
-         self.tt_path == tt_path
-
-
-    def __init__(self):
-        self.scoring = scoring
-        self.tt = {{}}
+    def __init__(self,tt_path, max_depth, suggested_move):
+        self.tt_path = tt_path
+        self.max_depth = max_depth
+        self.suggested_move = suggested_move
+         
+        # Make transposition table file and load it
+        file_path = Path(self.tt_path)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(self.tt_path, 'rb') as f:
+            try:
+                self.tt = pickle.load(f)
+            except:
+                self.tt = {}
+                
+    def save_tt(self):
+        with open(self.tt_path, 'wb') as f:
+            self.tt = pickle.dump(self.tt, f)
 
     def get_best_move(self):
         pass
 
-    def negamax(self, s_game:Sim_Game, depth, o_depth, alpha=float('infinity'), beta=-float('infinity'), tt=None, color=1):
+    def negamax(self, s_game:Sim_Game, depth, alpha=inf, beta=-inf, color=1):
         '''
-        Converted from Wikipedia Negamax algorithm: https://en.wikipedia.org/wiki/Negamax
+        Converted from the [Wikipedia page for Negamax with alpha beta pruning https://en.wikipedia.org/wiki/Negamax
         '''
         alpha_orig = alpha
         gp = s_game.get_gp_as_str()
@@ -103,48 +116,66 @@ class Algorithm:
         # transposition table lookup
         tt_entry = self.tt.get(gp)
         if tt_entry and tt_entry['depth'] >= depth:
-             if tt_entry['flag'] == Algorithm.EXACT:
-                  return tt_entry['value']
-             elif tt_entry['flag'] == Algorithm.LOWERBOUND:
-                  alpha = max(alpha, tt_entry['value'])
+            if tt_entry['flag'] == Algorithm.EXACT:
+                if depth == self.max_depth:
+                    self.suggested_move = tt_entry['move']
+                return tt_entry['value']
+            elif tt_entry['flag'] == Algorithm.LOWERBOUND:
+                alpha = max(alpha, tt_entry['value'])
+            elif tt_entry['flag'] == Algorithm.UPPERBOUND:
+                beta = min(beta, tt_entry['value'])
 
-             if alpha >= beta:
-                  return tt_entry['value']
+            if alpha >= beta:
+                if depth == self.max_depth:
+                    self.suggested_move = tt_entry['move']
+                return tt_entry['value']
         
-        if depth == 0 or s_game.is_end():
-            if s_game.isp1_win():
+        # Calculate node value
+        if depth == self.max_depth or s_game.is_end():
+            if s_game.is_p1_win():
                 value = 10
             elif s_game.is_p2_win():
                 value = -10
             else:
                 value = 0
-            return int(color*value+(1+depth*0.1))
+            return int(color*value+(1+depth*0.01))
 
         child_nodes = s_game.get_possible_moves()
-        value = -1*float('infinity')
+        best_value = -1*Algorithm.inf
+        best_move = []
         s_game_copy:Sim_Game = s_game.copy()
-        for move in child_modes:
+        for move in child_nodes:
             move_ = move.split(" ")
-            if move[0] == 'tap':
+            if move_[0] == 'tap':
                 valid = s_game_copy.move(type='tap',used_hand=move[1],target_hand=move[2])
             else:
                 valid = s_game_copy.move(type='split',rh_val=int(move[1]))
             if not valid:
-                 continue
+                continue
             
-            value= max(value, -self.negamax(s_game_copy, depth -1, -beta, alpha, -color))
-            alpha = max(alpha, value)
-            if alpha >= beta:
-                 break
+            value = -self.negamax(s_game_copy, depth -1, -beta, alpha, -color)
             
-            # store in transposition table
-            tt_entry = {}
-            tt_entry['value'] = value
-            if value <= alpha_orig:
-                tt_entry['flag'] = Algorithm.UPPERBOUND
-            elif value >= beta:
-                tt_entry['flag'] = Algorithm.LOWERBOUND
-            else:
-                tt_entry['flag'] = Algorithm.EXACT
-            tt_entry["depth"] = depth
-            self.tt[gp] = tt_entry
+            if best_value < value:
+                best_value = value
+                best_move = move_
+            
+            if alpha < value:
+                alpha = value
+                if depth == self.max_depth:
+                    self.suggested_move = tt_entry['move']
+                if alpha >= beta:
+                    break
+            
+        # store in transposition table
+        tt_entry = {}
+        tt_entry['value'] = best_value
+        if best_value <= alpha_orig:
+            tt_entry['flag'] = Algorithm.UPPERBOUND
+        elif best_value >= beta:
+            tt_entry['flag'] = Algorithm.LOWERBOUND
+        else:
+            tt_entry['flag'] = Algorithm.EXACT
+        tt_entry["depth"] = depth
+        tt_entry['move'] = best_move
+        self.tt[gp] = tt_entry
+        
